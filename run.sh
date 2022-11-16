d1cb08960be0a6b6e875dfa0cc4cda97faa75f4a
Sergii Domanskyi <sergii.domanskyi@jax.org>
2022-11-16 07:55:03 -0500

    update run parameters

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-samplesheet="./assets/samplesheet_test.csv"
-workdir="/fastscratch/domans/work"
+samplesheet="./assets/samplesheet_WM4237_ADD.csv"
+workdir="/fastscratch/domans/work_WM4237_ADD"
 
 sbatch job_sumner.sb ${samplesheet} ${workdir}
