26d5cf630033efcd28e5e518ec5695eaedffeafb
Sergii Domanskyi <sergii.domanskyi@jax.org>
2022-11-22 16:57:12 -0500

    update samplesheets

--- assets/id_script.py
+++ assets/id_script.py
@@ -1,12 +1,12 @@
 
-ids_WM4237_st = { 'WM4237_TE_S1_ST': 'SC2200092_WM4237-1-1337',
+ids_WM4237_st = {'WM4237_TE_S1_ST': 'SC2200092_WM4237-1-1337',
                 'WM4237_TE_S2_ST': 'SC2200093_WM4237-1-1337',
                 'WM4237_TE_S3_ST': 'SC2200094_WM4237-1-1337',
                 'WM4237_TE_S4_ST': 'SC2200095_WM4237-1-1337',
                 'WM4237_T0_S1_ST': 'SC2200319_WM4237-T0-Day0-197',
                 'WM4237_T0_S2_ST': 'SC2200320_WM4237-T0-Day0-197',
-                'WM4237_T2C_S1_ST': 'SC2200321_WM4237-CTRL-Day42-207',
-                'WM4237_T2C_S2_ST': 'SC2200322_WM4237-CTRL-Day42-207',
+                'WM4237_TC_S1_ST': 'SC2200321_WM4237-CTRL-Day42-207',
+                'WM4237_TC_S2_ST': 'SC2200322_WM4237-CTRL-Day42-207',
                 'WM4237_T1_S1_ST': 'SC2200323_WM4237-T1-Day14-229',
                 'WM4237_T1_S2_ST': 'SC2200324_WM4237-T1-Day14-229',
                 'WM4237_T2_S1_ST': 'SC2200325_WM4237-T2-Day42-211', 
@@ -26,27 +26,27 @@ for id in sorted(ids_WM4237_st.keys()):
 
 
          
-# ids_WM4007_st = { 'WM4237_T0C_S1_ST': 'SC2200092_WM4237-1-1337',
-#                 'WM4237_T0C_S2_ST': 'SC2200093_WM4237-1-1337',
-#                 'WM4237_T0C_S3_ST': 'SC2200094_WM4237-1-1337',
-#                 'WM4237_T0C_S4_ST': 'SC2200095_WM4237-1-1337',
-#                 'WM4237_T0_S1_ST': 'SC2200319_WM4237-T0-Day0-197',
-#                 'WM4237_T0_S2_ST': 'SC2200320_WM4237-T0-Day0-197',
-#                 'WM4237_T2C_S1_ST': 'SC2200321_WM4237-CTRL-Day42-207',
-#                 'WM4237_T2C_S2_ST': 'SC2200322_WM4237-CTRL-Day42-207',
-#                 'WM4237_T1_S1_ST': 'SC2200323_WM4237-T1-Day14-229',
-#                 'WM4237_T1_S2_ST': 'SC2200324_WM4237-T1-Day14-229',
-#                 'WM4237_T2_S1_ST': 'SC2200325_WM4237-T2-Day42-211', 
-#                 'WM4237_T2_S2_ST': 'SC2200326_WM4237-T2-Day42-211',
-#                 'WM4237_T3_S1_ST': 'SC2200327_WM4237-T3-Day70-221',
-#                 'WM4237_T3_S2_ST': 'SC2200328_WM4237-T3-Day70-221',
-#                 'WM4237_T4_S1_ST': 'SC2200329_WM4237-T4-Day94-214',
-#                 'WM4237_T4_S2_ST': 'SC2200330_WM4237-T4-Day94-214'}
-#
-# print('sample,image,grid')
-# for id in sorted(ids_WM4007_st.keys()):
-#     print('%s,/projects/chuang-lab/rubinstein/ST_melanomaPDX/data/%s/img/%s.tiff,/projects/chuang-lab/rubinstein/ST_melanomaPDX/data/%s/spaceranger/spatial/' \
-#           % (id, ids_WM4007_st[id], ids_WM4007_st[id].split('_')[0], ids_WM4007_st[id]))    
+ids_WM4007_st = {'WM4007_TC_S1_ST': 'SC2200800_WM4007-Ctrl-439',
+                 'WM4007_TC_S2_ST': 'SC2200801_WM4007-Ctrl-439',                
+                 'WM4007_T0_S1_ST': 'SC2200802_WM4007-T0-484',
+                 'WM4007_T0_S2_ST': 'SC2200803_WM4007-T0-484',     
+                 'WM4007_T1_S1_ST': 'SC2200804_WM4007-T1-426',
+                 'WM4007_T1_S2_ST': 'SC2200805_WM4007-T1-426',               
+                 'WM4007_T2_S1_ST': 'SC2200806_WM4007-T2-437',
+                 'WM4007_T2_S2_ST': 'SC2200807_WM4007-T2-437',                 
+                 'WM4007_T3_S1_ST': 'SC2200808_WM4007-T3-17',
+                 'WM4007_T3_S2_ST': 'SC2200809_WM4007-T3-17',                
+                 'WM4007_T4_S1_ST': 'SC2200810_WM4007-T4-6',
+                 'WM4007_T4_S2_ST': 'SC2200811_WM4007-T4-6',
+                }
+
+print('sample,image,grid')
+for id in sorted(ids_WM4007_st.keys()):
+    print('%s,/projects/chuang-lab/rubinstein/ST_melanomaPDX/data/WM4007/%s/img/%s.tiff,/projects/chuang-lab/rubinstein/ST_melanomaPDX/data/WM4007/%s/spaceranger/spatial/' \
+          % (id, ids_WM4007_st[id], ids_WM4007_st[id].split('_')[0], ids_WM4007_st[id]))    )    
+
+
+
 
 
 
