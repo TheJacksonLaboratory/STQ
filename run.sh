0aa062fa96be9634c01fe517e0e08e3a38a49123
Sergii Domanskyi <sergii.domanskyi@jax.org>
2022-11-25 14:03:47 -0500

    cleaning up

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 samplesheet="./assets/samplesheet_WM4237_ADD.csv"
 workdir="/fastscratch/domans/work_WM4237_ADD"
+outdir="./result"
 
-sbatch job_sumner.sb ${samplesheet} ${workdir}
+sbatch job_sumner.sb ${samplesheet} ${workdir} ${outdir}
