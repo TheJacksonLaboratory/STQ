31218a1b0fd4455d9480fcad880a1cd0ed939588
Sergii Domanskyi <Sergii.Domanskyi@jax.org>
2024-02-13 08:18:28 -0500

    demo testing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 workflow="two_references" ### "two_references" "one_reference" "arbitrary_grid" "deconvolution_indices"
-samplesheet="./assets/samplesheet_two.csv"
+samplesheet="./assets/samplesheet_demo.csv"
 workdir="./work"
 outdir="./results"
 binddir="/projects/"
